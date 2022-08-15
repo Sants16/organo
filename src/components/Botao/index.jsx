ee02968cb2592a83fb9b2208b9f72a03a32ec4ae
Sants16 <jvsantosrocha980@gmail.com>
2022-08-15 19:08:17 -0300

    botao/preventDefault

---
+++ src/components/Botao/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import styles from './Botao.module.css'
+const Botao = ({children}) => {
+    return ( 
+        <button className={styles.botao}>{children}</button>
+     );
+}
+ 
+export default Botao;
--- src/components/Formulario/index.jsx
+++ src/components/Formulario/index.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Botao from '../Botao';
 import CampoTexto from '../CampoTexto';
 import ListaSuspensa from '../ListaSuspensa';
 import styles from './Formulario.module.css'
@@ -14,14 +15,22 @@ const Formulario = () => {
         'Inovação e Gestão'
     ]
 
+    function pararEventoPadrao(e){
+        e.preventDefault()
+        console.log('bruh')
+    }
+
     return ( 
         <section className={styles.formulario}>
-            <form>
+            <form onSubmit={pararEventoPadrao}>
             <h2>Preencha os dados para criar o card do colaborador</h2>
             <CampoTexto text='Nome:' name='nome' placeholder='Digite seu nome'/>
             <CampoTexto text='Cargo:' name='cargo' placeholder='Digite seu cargo'/>
             <CampoTexto text='Imagem:' name='imagem' placeholder='Digite o endereço da imagem'/>
             <ListaSuspensa label='Time' itens={times}/>
+            <Botao>
+                Criar Card
+            </Botao>
             </form>
         </section>
      );
